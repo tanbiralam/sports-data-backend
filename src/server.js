16e8cb568d15b4359bff76e37b1d9405303e674e
tanbiralam <itzmetanbir@gmail.com>
2026-03-01 12:24:10 +0530

    implement matches POST and GET routes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { matchRouter } from "./routes/matches.js";
 const app = express();
 const port = 8000;
 
@@ -8,6 +8,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Sports live data server is running");
 });
+
+app.use("/matches", matchRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}/`);
